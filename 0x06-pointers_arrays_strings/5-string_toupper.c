a53ea0bf9692321e75377d058ea44695beb69c9a
Eyouman <eyucopyandmovies@gmail.com>
2022-12-22 15:57:37 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,9 +13,9 @@ char *string_toupper(char *)
 
 	while (str[index])
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index -= 32;
-		index++;
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
 	}
 
 	return (str);
