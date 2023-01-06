c3fa0757437c6b9f27a6c8d0891acaa68992e02c
Eyouman <119453493+Eyouman@users.noreply.github.com>
2023-01-06 15:37:26 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,1 +1,20 @@
-tsmyname.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
